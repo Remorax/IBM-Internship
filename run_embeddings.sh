6ffad616c037410d1182cd0aa0dede9c93aaa044
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-07-16 00:34:48 +0530

    Changed embedding script for xlnet running

--- run_embeddings.sh
+++ run_embeddings.sh
@@ -1,9 +1,5 @@
-c=('data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' 
-  'data_distilbert.pkl' 'data_distilbert_nli.pkl' 'data_distilbert.pkl' 'data_distilbert_nli.pkl'
-  'data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' 
-  'data_distilbert.pkl' 'data_distilbert_nli.pkl' 'data_distilbert.pkl' 'data_distilbert_nli.pkl'
-  'data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' )
-n=(1024 1024 1024 1024 1024 1024 800 800 600 600 600 600 600 600 300 300 300 300 300 300)
+c=('data_xlnet.pkl' 'data_xlnet.pkl' 'data_xlnet.pkl' 'data_xlnet.pkl' 'data_xlnet.pkl')
+n=(1600 1024 800 512 300)
 
 for i in $(seq 0 $((${#c[*]}-1)))
 do
