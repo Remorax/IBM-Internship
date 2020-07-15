38961fdf0d247b1b482ce313c6523bc9f4aa006f
Remorax <vivekbalasundaram@gmail.com>
2020-07-15 12:00:30 -0700

    Pushing XLNet

---
+++ run_embeddings.sh
@@ -0,0 +1,12 @@
+c=('data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' 
+  'data_distilbert.pkl' 'data_distilbert_nli.pkl' 'data_distilbert.pkl' 'data_distilbert_nli.pkl'
+  'data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' 
+  'data_distilbert.pkl' 'data_distilbert_nli.pkl' 'data_distilbert.pkl' 'data_distilbert_nli.pkl'
+  'data_bert.pkl' 'data_roberta.pkl' 'data_bert_nli.pkl' 'data_roberta_nli.pkl' )
+n=(1024 1024 1024 1024 1024 1024 800 800 600 600 600 600 600 600 300 300 300 300 300 300)
+
+for i in $(seq 0 $((${#c[*]}-1)))
+do
+    echo ${c[i]} ${n[i]}
+done
+
