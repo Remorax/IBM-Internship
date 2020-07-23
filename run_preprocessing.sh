297b9fdde11e5773d02f5e36e51f00087bbcf6d4
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-07-23 15:34:45 +0530

    Pushing rootpath scripts

--- run_preprocessing.sh
+++ run_preprocessing.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-max_neighbours=(8 10 12 18 21 22 23)
+max_neighbours=(8 10 12 18 21 22 23 24)
 min_neighbours=(1 2 3)
 
 for max_limit in "${max_neighbours[@]}";
 do
 	for min_limit in "${min_neighbours[@]}";
 	do
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_resolvedv2.txt" python Attention_preprocessing.py $max_limit $min_limit data_resolvedv2.pkl test_resolvedv2.pkl
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_resolved_spellchecked.txt" python Attention_preprocessing.py $max_limit $min_limit data_resolved_spellchecked.pkl test_resolved_spellchecked.pkl
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_rootpath.txt" python Attention_preprocessing.py $max_limit $min_limit data_rootpath.pkl test_rootpath.pkl
 	done
 done
