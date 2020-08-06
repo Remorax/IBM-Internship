b4956d0140e87c553355a9fcf12e593f907635d0
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-08-06 22:11:41 +0530

    Adding validation scripts

---
+++ run_val.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+max_neighbours=(8 10 12 18 21 22 23 32 34)
+min_neighbours=(1)
+
+for max_limit in "${max_neighbours[@]}";
+do
+	for min_limit in "${min_neighbours[@]}";
+	do
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_val.txt" python Attention_val.py $max_limit $min_limit data_multi_rootpath.pkl
+	done
+done
