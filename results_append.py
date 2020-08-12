e37b0b1a0c5fc8ba8ff2f9dc1b2987948b1396ed
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-08-13 00:40:09 +0530

    Added concat property scripts

--- results_append.py
+++ results_append.py
@@ -1,16 +1,20 @@
-import os
+import os, re
 import operator
 
 mapping_dict = {
     "ent_prop": ["(avg, ent+prop)", ", Dot product of node with neighbours, softmax, weighted average, both entities and props"],
-    "sum": ["(sum)", ", Dot product of node with neighbours, softmax, weighted sum"],
+    "val": ["(val size = 2)", ", Optimized entity model"],
     "unsoftmax": ["(avg, no softmax)", ", Dot product of node with neighbours, weighted average"],
     "context": ["(only context)", ", Dot product of node with neighbours, softmax, weighted average, context is directly output"],
-    "normalize": ["(avg, normalized)", ", Dot product of node with neighbours, softmax, weighted average, normalized"],
+    
     "resolved": ["(self + v + min + unabbreviated)", ", Min neighbour filter, oversampled, Dot product of node with neighbours, softmax, dot with trainable param"],
     "unhas": ["(self + v + min + removes has from property)", ", Min neighbour filter, oversampled,  Dot product of node with neighbours, softmax, dot with trainable param"],
     "spellchecked": ["(self + v + min + spellchecked)", ", Min neighbour filter, oversampled, Dot product of node with neighbours, softmax, dot with trainable param"],
     "rootpath_multi": ["(includes multiple parent paths from root)", ", spellchecked, abbreviation resolved, has removed, oversampled, Dot product of node with neighbours, softmax, dot with trainable param"],
+    "att_sumnorm_dtpath\d+\_\d+\_prop": ["(property matching + sum normalized + datatype props)", ", optimized entity model + property matching (including datatype props) by passing domain and range through siamese followed by weighted sum with only 2 trainable params (third one is 1-sum), validation size=2"],
+    "att_dtpath\d+\_\d+\_prop": ["(property matching + datatype props)", ", optimized entity model + property matching (including datatype props) by passing domain and range through siamese followed by weighted sum, validation size=2"],
+    "att_sumnorm\d+\_\d+\_prop": ["(property matching + sum normalized)", ", optimized entity model + property matching (including datatype props) by passing domain and range through siamese followed by weighted sum with only 2 trainable params (third one is 1-sum), validation size=2"],
+    "att\d+\_\d+\_prop": ["(property matching)", ", optimized entity model + property matching by passing domain and range through siamese followed by weighted sum, validation size=2"],
     "rootpath": ["(includes parent path from root)", ", spellchecked, abbreviation resolved, has removed, oversampled, Dot product of node with neighbours, softmax, dot with trainable param"],
     "ae": ["(auto encoder)", ", Dot product of node with neighbours, softmax, weighted average, auto encoder"],
     "cross": ["(cross attention)", ", Dot product of neighbours with other entity's neighbours, softmax, weighted average"],
@@ -19,6 +23,8 @@ mapping_dict = {
     "hybrid": ["(cross + v + min)", ", Min neighbour filter, Dot product of neighbours with other entity's neighbours, softmax, dot with trainable param"],
     "self_cross": ["(self+cross)", ", Min neighbour filter, Dot product of neighbours with entity AND other entity's neighbours, softmax, dot with trainable param"],
     "v": ["(trainable param)", ", Dot product of node with neighbours, softmax, dot with trainable param"],
+    "sum": ["(sum)", ", Dot product of node with neighbours, softmax, weighted sum"],
+    "normalize": ["(avg, normalized)", ", Dot product of node with neighbours, softmax, weighted average, normalized"],
     "default": ["", ", Dot product of node with neighbours, softmax, weighted average"]
 }
 
@@ -27,19 +33,25 @@ for file in os.listdir("."):
     if file.startswith("Output_att"):
         neighbours = [''.join(filter(str.isdigit, num)) for num in file.split("_")]
         neighbours = [el for el in neighbours if el]
-        intent = "USE + dot Attn + " + ",".join(neighbours) + " neighbours + Cos Sim "
+        intent = "VeeAlign (entity) + " + ",".join(neighbours) + " neighbours"
         try:
-            threshold = str(round(float([l.split()[-1] for l in open(file).read().split("\n") if "Best threshold:" in l][0]), 3))
+            threshold = str(round(float([l.split()[-1] for l in open(file).read().split("\n") if "Threshold:" in l][-1]), 3))
         except:
             print (file)
             continue
         for elem in mapping_dict:
-            if elem in file or elem == "default":
+            if re.search(elem, file) or elem == "default":
+
                 key = mapping_dict[elem][0]
                 desc = "Optimum threshold " + threshold + mapping_dict[elem][1]
+                print (file, key)
                 break
         intent += key
-        results = [l for l in open(file).read().split("\n") if "Final Results:" in l][0]  
+        try:
+            results = [l for l in open(file).read().split("\n") if "Final Results:" in l][0]  
+        except:
+            print ("no results", file)
+            continue
         results = results.split("[")[1].split("]")[0].strip().split()
         line = "\t".join([intent] + results + [desc])
         final.append((line, key, tuple([int(el) for el in neighbours])))
--- run_german.sh
+++ run_german.sh
@@ -7,9 +7,7 @@ for max_limit in "${max_neighbours[@]}";
 do
 	for min_limit in "${min_neighbours[@]}";
 	do
-	        jbsub -q x86_24h -mem 40g -require v100 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_german.txt" python Attention_german.py $max_limit $min_limit data_german_dataset.pkl "test_german_dataset"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require v100 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_germanv2.txt" python Attention_german.py $max_limit $min_limit data_german_datasetv2.pkl "test_german_datasetv2"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_german.txt" python Attention_german.py $max_limit $min_limit data_german_dataset.pkl "test_german_dataset"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_germanv2.txt" python Attention_german.py $max_limit $min_limit data_german_datasetv2.pkl "test_german_datasetv2"$max_limit"_"$min_limit".pkl"
+		jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_german.txt" python Attention_german.py $max_limit $min_limit data_german_dataset.pkl "test_german_dataset"$max_limit"_"$min_limit".pkl"
+		jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_germanv2.txt" python Attention_german.py $max_limit $min_limit data_german_datasetv2.pkl "test_german_datasetv2"$max_limit"_"$min_limit".pkl"
 	done
 done
