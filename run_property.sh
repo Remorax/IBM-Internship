93a5a1facae98e5d096249792fb4f5700713d33b
Harshit Kumar harshitk@in.ibm.com <harshitk@dccxl003.pok.ibm.com>
2020-08-12 07:58:54 -0400

    job executed on Aug 12 run_property.sh

--- run_property.sh
+++ run_property.sh
@@ -7,9 +7,9 @@ for max_limit in "${max_neighbours[@]}";
 do
 	for min_limit in "${min_neighbours[@]}";
 	do
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop.txt" python Attention_prop.py $max_limit $min_limit data_prop.pkl "test_prop"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_dtpath"$max_limit"_"$min_limit"_prop.txt" python Attention_prop.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_dtpath"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_sumnorm"$max_limit"_"$min_limit"_prop.txt" python Attention_prop_sumnormalized.py $max_limit $min_limit data_prop.pkl "test_prop_sumnorm"$max_limit"_"$min_limit".pkl"
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_sumnorm_dtpath"$max_limit"_"$min_limit"_prop.txt" python Attention_prop_sumnormalized.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_sumnorm_dtpath"$max_limit"_"$min_limit".pkl"
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_prop.py $max_limit $min_limit data_prop.pkl "test_prop"$max_limit"_"$min_limit".pkl"
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_dtpath"$max_limit"_"$min_limit"_prop.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_prop.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_dtpath"$max_limit"_"$min_limit".pkl"
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_sumnorm"$max_limit"_"$min_limit"_prop.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_prop_sumnormalized.py $max_limit $min_limit data_prop.pkl "test_prop_sumnorm"$max_limit"_"$min_limit".pkl"
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att_sumnorm_dtpath"$max_limit"_"$min_limit"_prop.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_prop_sumnormalized.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_sumnorm_dtpath"$max_limit"_"$min_limit".pkl"
 	done
 done
