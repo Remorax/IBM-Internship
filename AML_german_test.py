6cdf485e537fdff1673f1395e8ed812f7e870ba1
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-08-14 23:39:13 +0530

    Fixes indentation bug

--- AML_german_test.py
+++ AML_german_test.py
@@ -4,7 +4,7 @@ ontologies_in_alignment = pickle.load(open("data_german_dataset_phrase.pkl", "rb
 ontologies_in_alignment_new = [tuple([el.replace("german_datasets/mapping ", "german_datasets_copy/") for el in l]) for l in ontologies_in_alignment][2:]
 
 for ont_pair in ontologies_in_alignment_new:
-	print ("Doing {}...".format(ont_pair))
+    print ("Doing {}...".format(ont_pair))
     t = time.time()
     a, b, c = ont_pair[0], ont_pair[1], ont_pair[0].split("/")[-1].rsplit(".",1)[0].replace(".", "_").lower() + "-" + ont_pair[1].split("/")[-1].rsplit(".",1)[0].replace(".", "_").lower()
     java_command = "java -jar AML_v3.1/AgreementMakerLight.jar -s " + a + " -t " + b + " -o AML-test-results/" + c + ".rdf -a"
