b31ea71fe5b9860f002b4f394d76d6e6e9bb7c10
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-08-15 02:02:32 +0530

    Adds v100 gpu in 6d queue

--- run_property.sh
+++ run_property.sh
@@ -7,7 +7,9 @@ for max_limit in "${max_neighbours[@]}";
 do
 	for min_limit in "${min_neighbours[@]}";
 	do
-        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat_sparse.txt" python Attention_prop_concat.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop"$max_limit"_"$min_limit".pkl"
-        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat.txt" python Attention_prop_concat_sparse.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_dtpath"$max_limit"_"$min_limit".pkl"
+        jbsub -q x86_6d -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat_sparse.txt" python Attention_prop_concat.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop"$max_limit"_"$min_limit".pkl"
+        jbsub -q x86_6d -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat.txt" python Attention_prop_concat_sparse.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_dtpath"$max_limit"_"$min_limit".pkl"
+        jbsub -q x86_6d -mem 40g -require v100 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat_sparse.txt" python Attention_prop_concat.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop"$max_limit"_"$min_limit".pkl"
+        jbsub -q x86_6d -mem 40g -require v100 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_prop_concat.txt" python Attention_prop_concat_sparse.py $max_limit $min_limit data_prop_dtpath.pkl "test_prop_dtpath"$max_limit"_"$min_limit".pkl"
 	done
 done
