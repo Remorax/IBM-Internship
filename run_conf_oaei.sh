cc5763ea83dde26f89092e056c851dbda70c3da5
Harshit Kumar harshitk@in.ibm.com <harshitk@dccxl004.pok.ibm.com>
2020-09-06 02:49:23 -0400

    oaei conference

--- run_conf_oaei.sh
+++ run_conf_oaei.sh
@@ -10,24 +10,12 @@ for max_pathlen in "${max_pathlens[@]}";
 do
 	for max_path in "${max_paths[@]}";
 	do
-		jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml_bagofnbrs.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_aml_bagofnbrs_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_aml_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pt"
-		jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml_bagofnbrs_wtpath.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei_weighted.py Input/data_conf_oaei_german_aml_bagofnbrs_thresh.pkl $max_path $max_pathlen $threshold "Output/test_conf_oaei_german_aml_wtpath_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml_wtpath_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pt"
-	done
-done
-
-max_pathlens=(4 5 8)
-max_paths=(13 21 49)
-thresholds=(0.66 0.735 0.736 0.8 0.96 0.98)
-
-for max_pathlen in "${max_pathlens[@]}";
-do
-	for max_path in "${max_paths[@]}";
-	do
-		for threshold in "${thresholds[@]}";
+        	for threshold in "${thresholds[@]}";
 		do
-			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_aml_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_aml"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml"$max_path"_"$max_pathlen"_"$threshold".pt"
-			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml_wtpath.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei_weighted.py Input/data_conf_oaei_german_aml_thresh.pkl $max_path $max_pathlen $threshold "Output/test_conf_oaei_german_aml_wtpath"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml_wtpath"$max_path"_"$max_pathlen"_"$threshold".pt"
+			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml_bagofnbrs.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_aml_bagofnbrs_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_aml_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pt"
+			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_aml_bagofnbrs_wtpath.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei_weighted.py Input/data_conf_oaei_german_aml_bagofnbrs_thresh.pkl $max_path $max_pathlen $threshold "Output/test_conf_oaei_german_aml_wtpath_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_aml_wtpath_bagofnbrs"$max_path"_"$max_pathlen"_"$threshold".pt"
 		done
 	done
 done
+
 
