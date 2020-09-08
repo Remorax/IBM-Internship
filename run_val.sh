0c7f1fcc69543c0987ca04af27aaf2f7e0ba7fbd
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-09-09 00:58:20 +0530

    Fixes paths

--- run_val.sh
+++ run_val.sh
@@ -7,6 +7,6 @@ for max_limit in "${max_neighbours[@]}";
 do
 	for min_limit in "${min_neighbours[@]}";
 	do
-	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Output_att"$max_limit"_"$min_limit"_onestep.txt" python Attention_val.py $max_limit $min_limit data_multi_rootpath.pkl
+	        jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_limit"_"$min_limit"_onestep.txt" python Attention_val.py $max_limit $min_limit Input/data_multi_rootpath.pkl
 	done
 done
