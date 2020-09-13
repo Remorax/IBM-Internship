2eb29e461e173fd9245280d8e072351c7424b24a
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-09-13 21:58:36 +0530

    merges aml w logmap

--- run_conf_oaei.sh
+++ run_conf_oaei.sh
@@ -17,4 +17,22 @@ do
 	done
 done
 
+# max_paths=(3 5 7 12 18 21 24 26)
+max_paths=(7 18)
+# max_pathlens=(1 3 4 6 8)
+max_pathlens=(3 6 8)
+# thresholds=(0.29 0.47 0.49 0.51 0.54 0.62 0.7 0.71 0.666 0.694 0.735)
+thresholds=(0.29 0.5 0.51 0.69 0.7 0.74 0.75 0.77 0.78 0.79 0.8 0.82 0.83)
+
+for max_pathlen in "${max_pathlens[@]}";
+do
+	for max_path in "${max_paths[@]}";
+	do
+		for threshold in "${thresholds[@]}";
+		do
+			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_logmap.txt" python Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_logmap_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pt"
+		done
+	done
+done
+
 
--- run_conf_oaei_logmap.sh
+++ run_conf_oaei_logmap.sh
@@ -15,7 +15,7 @@ do
 	do
 		for threshold in "${thresholds[@]}";
 		do
-			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_logmap.txt" ~/anaconda2/envs/myenv/bin/python3.6 Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_logmap_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pt"
+			jbsub -q x86_24h -mem 40g -require k80 -cores 1x1+1 -out "Results/Output_att"$max_path"_"$max_pathlen"_"$threshold"_conf_oaei_german_logmap.txt" python Attention_german_amlconf_oaei.py Input/data_conf_oaei_german_logmap_thresh.pkl $max_path $max_pathlen $threshold  "Output/test_conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pkl" "Models/conf_oaei_german_logmap_"$max_path"_"$max_pathlen"_"$threshold".pt"
 		done
 	done
 done
